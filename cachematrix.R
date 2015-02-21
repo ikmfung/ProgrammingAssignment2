ab87c3b0daca0d30a1fe56c6359d6f0841657d7d
ikmfung <ikmfung@uwaterloo.ca>
2015-02-21 02:19:23 -0500

    assignment submission 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function demonstrates the lexical scoping 
+## functionality within R, the function takes a matrix, 
+## computes the inverse of the matrix, and then caches 
+## the inverse of the matrix. This is possible because of using the superassignment operation (<<-), we're able to 
 
-## Write a short comment describing this function
+## makeCacheMatrix is the parent environment of the 
+## function, using the superassignment (<<-) as null 
+## in order to allow the inverse matrix to be cached 
+## as (m) when solved 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve function calculates the inverse of 
+## our matrix object, based on lexical scoping, 
+## m becomes null due to makeCacheMatrix, and then 
+## solveCache matrix is able to calculate the inverse 
+## and store the inverse as cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
+
